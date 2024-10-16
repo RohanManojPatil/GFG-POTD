9d36fdda495fc1947de86bcabe1754372d9735d5
RohanManojPatil <144838652+RohanManojPatil@users.noreply.github.com>
2024-10-16 22:05:20 +0530

    Add files via upload

---
+++ October-2024/16-10-2024.cpp
@@ -0,0 +1,27 @@
+
+*******************************GFG POTD******************************
+*	               T.C = O(N)							                * 
+*	               							                        * 
+*	               S.C = O(1)                                           * 
+*	                                                                * 
+*********************************************************************
+class Solution {
+  public:
+    bool checkSorted(vector<int> &arr) {
+        // code here.
+        int cnt = 0;
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i] != i+1)
+            {
+                swap(arr[i],arr[arr[i]-1]);
+                cnt++;
+                i--;
+            }
+            if(cnt>2)
+                return false;
+        }
+        
+        return cnt==0 or cnt==2;
+    }
+};
