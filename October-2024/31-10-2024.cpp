c38c1e47dbf447fece6cc601d2cdac8b9c4d9929
Rohan Patil <144838652+RohanManojPatil@users.noreply.github.com>
2024-11-01 01:32:43 +0530

    Update 31-10-2024.cpp

--- October-2024/31-10-2024.cpp
+++ October-2024/31-10-2024.cpp
@@ -1,7 +1,59 @@
 
 *******************************GFG POTD******************************
-*	               T.C = 							                * 
+*	               T.C = O(N)							                * 
 *	               							                        * 
-*	               S.C =                                            * 
+*	               S.C = O(1)                                           * 
 *	                                                                * 
 *********************************************************************
+/*structure of the node of the DLL is as
+struct Node {
+    int data;
+    struct Node* prev, *next;
+};
+*/
+// function should insert a new node in sorted way in
+// a sorted doubly linked list
+// Return the head after insertion
+class Solution {
+  public:
+    Node* sortedInsert(Node* head, int x) {
+        Node *node = getNode(x);
+        
+        if(x<=head->data)
+        {
+            node->next = head;
+            head->prev = node;
+            head = node;
+        }
+        else
+        {
+            Node* prev = head, *curr = head->next;
+            
+            while(prev != NULL && curr != NULL)
+            {
+                if(prev->data <= x && x <= curr->data)
+                {
+                    break;
+                }
+                
+                prev = curr;
+                curr = curr->next;
+            }
+            
+            if(curr == NULL)
+            {
+                prev->next = node;
+                node->prev = prev;
+            }
+            else
+            {
+                prev->next = node;
+                node->prev = prev;
+                node->next = curr;
+                curr->prev = node;
+            }
+        }
+        
+        return head;
+    }
+};
