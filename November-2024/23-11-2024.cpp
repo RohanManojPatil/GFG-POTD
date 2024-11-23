6f5858922f85907b77c2ac29de29bb9e6cda8373
Rohan Patil <144838652+RohanManojPatil@users.noreply.github.com>
2024-11-23 21:47:45 +0530

    Add files via upload

---
+++ November-2024/23-11-2024.cpp
@@ -0,0 +1,23 @@
+*******************************GFG POTD*************************
+*	               T.C = O(n)							                * 
+*	               							                        * 
+*	               S.C = O(1)                                           * 
+*	                                                                * 
+*********************************************************************
+// User function Template for C++
+
+class Solution {
+  public:
+    int getMinDiff(int k, vector<int> &arr) {
+        int n=arr.size();
+        sort(arr.begin() , arr.end()) ; 
+        int ans = arr[n-1] - arr[0] ; 
+        for(int i=1;i<n;i++){
+            int mini = min(arr[0]+k , arr[i]-k) ; 
+            int maxi = max(arr[i-1]+k , arr[n-1]-k) ; 
+            ans = min(ans , maxi - mini) ; 
+        }
+        
+        return ans; 
+    }
+};
