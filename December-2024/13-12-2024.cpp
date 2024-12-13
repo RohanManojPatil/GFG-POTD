706f201ea6550bfda8fcdfa7913fff28aaf6774b
Rohan Patil <144838652+RohanManojPatil@users.noreply.github.com>
2024-12-13 10:09:54 +0530

    Add files via upload

---
+++ December-2024/13-12-2024.cpp
@@ -0,0 +1,19 @@
+*******************************GFG POTD*************************
+*	               T.C = O(n)							                * 
+*	               							                        * 
+*	               S.C = O(1)                                           * 
+*	                                                                * 
+*********************************************************************
+class Solution {
+  public:
+    int findMin(vector<int>& arr) {
+         // complete the function here
+        int min = arr[0];
+        for(int i=0;i<arr.size(); i++){
+            if(arr[i]<min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+};
