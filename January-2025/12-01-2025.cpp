6264b5bd289b0bc406a035f954ac5d08a2bfb751
Rohan Patil <144838652+RohanManojPatil@users.noreply.github.com>
2025-01-13 23:50:55 +0530

    Add files via upload

---
+++ January-2025/12-01-2025.cpp
@@ -0,0 +1,33 @@
+*******************************GFG POTD*************************
+*	               T.C = O(n)							                * 
+*	               							                        * 
+*	               S.C =  O(1)                                          * 
+*	                                                                * 
+*********************************************************************
+class Solution {
+  public:
+    int maxWater(vector<int> &arr) {
+        int i = 0, j = arr.size() - 1;
+        int leftMax = 0, rightMax = 0;
+        int totalWater = 0;
+        
+        while(i < j)
+        {
+            leftMax = max(leftMax, arr[i]);
+            rightMax = max(rightMax, arr[j]);
+            
+            if(arr[i] <= arr[j])
+            {
+                totalWater += leftMax - arr[i];
+                i++;
+            }
+            else
+            {
+                totalWater += rightMax - arr[j];
+                j--;
+            }
+        }
+        
+        return totalWater;
+    }
+};
