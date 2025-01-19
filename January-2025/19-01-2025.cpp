0cdf7fa6c0d7a7dce8bd1345226931a90159cf8a
Rohan Patil <144838652+RohanManojPatil@users.noreply.github.com>
2025-01-19 19:51:10 +0530

    Add files via upload

---
+++ January-2025/19-01-2025.cpp
@@ -0,0 +1,52 @@
+*******************************GFG POTD*************************
+*	               T.C = O(N)							                * 
+*	               							                        * 
+*	               S.C = O(1)                                           * 
+*	                                                                * 
+*********************************************************************
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution {
+  public:
+    // Function to rotate a linked list.
+    Node* rotate(Node* head, int k) {
+        int count = 0;
+        Node* newHead = NULL;
+        Node* temp = head;
+        Node* prev = NULL;
+        while(count != k)
+        {
+            prev = temp;
+            temp = temp->next;
+            count++;
+        }
+        
+        if(temp == NULL)
+        {
+            return head;
+        }
+        
+        prev->next = NULL;
+        newHead = temp;
+        
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        
+        temp->next = head;
+        
+        return newHead;
+    }
+};
